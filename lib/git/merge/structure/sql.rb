954548cb4e73c7c82c38c6a456ae6a61c3cbcb26
Akinori MUSHA <knu@idaemons.org>
2018-11-28 23:06:06 +0900

    bundle gem --exe git-merge-structure-sql

---
+++ lib/git/merge/structure/sql.rb
@@ -0,0 +1,11 @@
+require "git/merge/structure/sql/version"
+
+module Git
+  module Merge
+    module Structure
+      module Sql
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/git/merge/structure/sql/version.rb
@@ -0,0 +1,9 @@
+module Git
+  module Merge
+    module Structure
+      module Sql
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
